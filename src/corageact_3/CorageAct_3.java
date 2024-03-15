83f424e52a2fae85faee4e406f61f759cc3c01dd
Lenovo <Lenovo@LAPTOP-H5VF19G9>
2024-03-15 14:25:20 +0800

    Activity 3

---
+++ src/corageact_3/CorageAct_3.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package corageact_3;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CorageAct_3 {
+
+    
+    public static void main(String[] args) {
+        double Celsius = 23;
+        double Fahrenheit = (Celsius * 9/5) + 32;
+        
+        System.out.println("Temperature in Fahrenheit is: " + Fahrenheit + "F");
+    }
+    
+}
